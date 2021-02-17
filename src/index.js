a8ab6ff0187df6907eca0ac4927ab43d4647c330
AikoSuzuki <g18947sa@gm.tsuda.ac.jp>
2021-02-17 21:10:54 +0900

    committing functionadd3

--- src/index.js
+++ src/index.js
@@ -134,7 +134,10 @@
 
 	/* 演習1 ここまで */
 
-	/* TODO: 演習2 以降は下記に自由に記入 */
+        /* TODO: 演習2 以降は下記に自由に記入 */
+        function add(){
+	    eqlNum.value = parseFloat(midNum[0].value) + parseFloat(midNum[1].value);
+	}
 
 	/* 演習の編集範囲 おわり */
 
